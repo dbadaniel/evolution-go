d366a12262075375d72a3be15ed90c459a37b438
dbadaniel <dbadaniel@gmail.com>
2026-07-06 19:33:41 -0300

    upstream: use buttons message for replies

--- pkg/sendMessage/service/send_service.go
+++ pkg/sendMessage/service/send_service.go
@@ -192,6 +192,8 @@ type ButtonStruct struct {
 	Footer       string       `json:"footer"`
 	Id           string       `json:"id"`
 	Buttons      []Button     `json:"buttons"`
+	ImageUrl     string       `json:"imageUrl,omitempty"`
+	VideoUrl     string       `json:"videoUrl,omitempty"`
 	Delay        int32        `json:"delay"`
 	MentionedJID []string     `json:"mentionedJid"`
 	MentionAll   bool         `json:"mentionAll"`
@@ -1754,7 +1756,7 @@ func mapKeyType(keyType string) string {
 }
 
 func (s *sendService) SendButton(data *ButtonStruct, instance *instance_model.Instance) (*MessageSendStruct, error) {
-	_, err := s.ensureClientConnected(instance.Id)
+	client, err := s.ensureClientConnected(instance.Id)
 	if err != nil {
 		return nil, err
 	}
@@ -1881,6 +1883,7 @@ func (s *sendService) SendButton(data *ButtonStruct, instance *instance_model.In
 	_, _ = cryptoRand.Read(msgSecret)
 
 	var msg *waE2E.Message
+	msgType := "InteractiveMessage"
 
 	if hasPix {
 		paymentMsgParams := `{"native_flow_name":"order_details","version":1}`
@@ -1947,9 +1950,79 @@ func (s *sendService) SendButton(data *ButtonStruct, instance *instance_model.In
 		}
 
 		if hasReply {
-			msg = &waE2E.Message{
-				InteractiveMessage: interactiveMsg,
+			var replyButtons []*waE2E.ButtonsMessage_Button
+			for _, v := range data.Buttons {
+				replyButtons = append(replyButtons, &waE2E.ButtonsMessage_Button{
+					ButtonID: proto.String(v.Id),
+					ButtonText: &waE2E.ButtonsMessage_Button_ButtonText{
+						DisplayText: proto.String(v.DisplayText),
+					},
+					Type: waE2E.ButtonsMessage_Button_RESPONSE.Enum(),
+				})
 			}
+
+			buttonsMsg := &waE2E.ButtonsMessage{
+				ContentText: proto.String(data.Description),
+				FooterText:  proto.String(data.Footer),
+				HeaderType:  waE2E.ButtonsMessage_EMPTY.Enum(),
+				Buttons:     replyButtons,
+			}
+
+			if data.ImageUrl != "" {
+				if resp, err := http.Get(data.ImageUrl); err == nil {
+					fileData, readErr := io.ReadAll(resp.Body)
+					resp.Body.Close()
+					if readErr == nil {
+						if uploaded, upErr := client.Upload(context.Background(), fileData, whatsmeow.MediaImage); upErr == nil {
+							buttonsMsg.HeaderType = waE2E.ButtonsMessage_IMAGE.Enum()
+							buttonsMsg.Header = &waE2E.ButtonsMessage_ImageMessage{
+								ImageMessage: &waE2E.ImageMessage{
+									URL:           proto.String(uploaded.URL),
+									DirectPath:    proto.String(uploaded.DirectPath),
+									MediaKey:      uploaded.MediaKey,
+									Mimetype:      proto.String("image/jpeg"),
+									FileEncSHA256: uploaded.FileEncSHA256,
+									FileSHA256:    uploaded.FileSHA256,
+									FileLength:    proto.Uint64(uint64(len(fileData))),
+								},
+							}
+						}
+					}
+				}
+			} else if data.VideoUrl != "" {
+				if resp, err := http.Get(data.VideoUrl); err == nil {
+					fileData, readErr := io.ReadAll(resp.Body)
+					resp.Body.Close()
+					if readErr == nil {
+						if uploaded, upErr := client.Upload(context.Background(), fileData, whatsmeow.MediaVideo); upErr == nil {
+							buttonsMsg.HeaderType = waE2E.ButtonsMessage_VIDEO.Enum()
+							buttonsMsg.Header = &waE2E.ButtonsMessage_VideoMessage{
+								VideoMessage: &waE2E.VideoMessage{
+									URL:           proto.String(uploaded.URL),
+									DirectPath:    proto.String(uploaded.DirectPath),
+									MediaKey:      uploaded.MediaKey,
+									Mimetype:      proto.String("video/mp4"),
+									FileEncSHA256: uploaded.FileEncSHA256,
+									FileSHA256:    uploaded.FileSHA256,
+									FileLength:    proto.Uint64(uint64(len(fileData))),
+								},
+							}
+						}
+					}
+				}
+			}
+
+			msg = &waE2E.Message{
+				DocumentWithCaptionMessage: &waE2E.FutureProofMessage{
+					Message: &waE2E.Message{
+						ButtonsMessage: buttonsMsg,
+					},
+				},
+				MessageContextInfo: &waE2E.MessageContextInfo{
+					MessageSecret: msgSecret,
+				},
+			}
+			msgType = "ButtonsMessage"
 		} else {
 			msg = &waE2E.Message{
 				DocumentWithCaptionMessage: &waE2E.FutureProofMessage{
@@ -1999,7 +2072,7 @@ func (s *sendService) SendButton(data *ButtonStruct, instance *instance_model.In
 		})
 	}
 
-	messaged, err := s.SendMessage(instance, msg, "InteractiveMessage", &SendDataStruct{
+	messaged, err := s.SendMessage(instance, msg, msgType, &SendDataStruct{
 		Id:              data.Id,
 		Number:          data.Number,
 		Quoted:          data.Quoted,
@@ -2316,6 +2389,22 @@ func (s *sendService) SendMessage(instance *instance_model.Instance, msg *waE2E.
 					QuotedMessage: &waE2E.Message{Conversation: proto.String("")},
 				}
 			}
+		case "ButtonsMessage":
+			if m.ButtonsMessage != nil {
+				m.ButtonsMessage.ContextInfo = &waE2E.ContextInfo{
+					StanzaID:      proto.String(data.Quoted.MessageID),
+					Participant:   proto.String(data.Quoted.Participant),
+					QuotedMessage: &waE2E.Message{Conversation: proto.String("")},
+				}
+			} else if msg.DocumentWithCaptionMessage != nil &&
+				msg.DocumentWithCaptionMessage.Message != nil &&
+				msg.DocumentWithCaptionMessage.Message.ButtonsMessage != nil {
+				msg.DocumentWithCaptionMessage.Message.ButtonsMessage.ContextInfo = &waE2E.ContextInfo{
+					StanzaID:      proto.String(data.Quoted.MessageID),
+					Participant:   proto.String(data.Quoted.Participant),
+					QuotedMessage: &waE2E.Message{Conversation: proto.String("")},
+				}
+			}
 		default:
 			return nil, fmt.Errorf("invalid messageType: %s", messageType)
 		}
@@ -2354,6 +2443,8 @@ func (s *sendService) SendMessage(instance *instance_model.Instance, msg *waE2E.
 			// ContextInfo already set in SendCarousel/SendButton/SendList
 		case "ListMessage":
 			// ContextInfo already set in SendList
+		case "ButtonsMessage":
+			// ContextInfo already set in SendButton
 		default:
 			return nil, fmt.Errorf("invalid messageType: %s", messageType)
 		}
